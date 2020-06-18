7553f573a57b07e272fd23538e88cea8e79d5848
LostLaser <jtgolds6@gmail.com>
2020-06-17 23:54:44 -0500

    Switching to websocket communication

--- handlers.go
+++ handlers.go
@@ -3,56 +3,71 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
+	cluster "github.com/LostLaser/recoverE"
 	"github.com/gorilla/websocket"
 )
 
-//func electionView(rw http.ResponseWriter, req *http.Request) {
-//	// Make sure that the writer supports flushing.
-//	//
-//	flusher, ok := rw.(http.Flusher)
-//
-//	if !ok {
-//		http.Error(rw, "Streaming unsupported!", http.StatusInternalServerError)
-//		return
-//	}
-//
-//	rw.Header().Set("Content-Type", "text/event-stream")
-//	rw.Header().Set("Cache-Control", "no-cache")
-//	rw.Header().Set("Connection", "keep-alive")
-//	rw.Header().Set("Access-Control-Allow-Origin", "*")
-//
-//	c := cluster.New(2, time.Second*2)
-//	defer c.Purge()
-//
-//	for {
-//		// Write to the ResponseWriter
-//		// Server Sent Events compatible
-//		fmt.Fprintf(rw, "data: %s\n\n", c.ReadEvent())
-//
-//		// Flush the data immediatly instead of buffering it for later.
-//		flusher.Flush()
-//	}
-//}
+func electionView(w http.ResponseWriter, r *http.Request) {
 
-func electionView(rw http.ResponseWriter, req *http.Request) {
+	// set up websocket based off request
 	u := websocket.Upgrader{}
-	c, err := u.Upgrade(rw, req, nil)
+	u.CheckOrigin = func(r *http.Request) bool { return true }
+	conn, err := u.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println(err)
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
 		return
 	}
 
-	// receive message
-	messageType, message, err := c.ReadMessage()
+	// create cluster
+	keys := r.URL.Query()
+	count, err := strconv.Atoi(keys.Get("count"))
 	if err != nil {
-		log.Println(err)
+		http.Error(w, "Query parameter 'count' missing or invalid", http.StatusBadRequest)
 	}
-	log.Println(message)
 
-	// send message
-	err = c.WriteMessage(messageType, []byte("HEY"))
+	socketMessaging(conn, count)
+}
+
+func socketMessaging(conn *websocket.Conn, count int) {
+	c := cluster.New(count, time.Second*4)
+	defer c.Purge()
+	defer conn.Close()
+
+	ids := c.ServerIds()
+	err := conn.WriteJSON(constructMessage("setup", ids))
 	if err != nil {
 		log.Println(err)
 	}
+
+	// receive messages
+	go func() {
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			log.Println(message)
+		}
+	}()
+
+	// stream cluster events to client
+	for {
+		err := conn.WriteJSON(constructMessage("event", c.ReadEvent()))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func constructMessage(operation string, payload interface{}) map[string]interface{} {
+	m := make(map[string]interface{})
+	m["operation"] = operation
+	m["payload"] = payload
+
+	return m
 }
